3bec04cb15061e8ce0622b10290f1fbc4f480a0f
Yannstj <yannick.saintjust@gmail.com>
2025-04-08 01:00:41 +0200

    correcting responsive for about desc

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import js from '@eslint/js';
-import globals from 'globals';
+import prettier from 'eslint-config-prettier';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
 
 export default [
   { ignores: ['dist'] },
@@ -26,5 +27,6 @@ export default [
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
+    prettier,
   },
 ];
